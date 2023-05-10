e960318c045f62555299e18116a30e862139d6c1
Tagellajore <tagelsft9@gmail.com>
2023-05-10 16:37:56 -0400

    first commit on a multiplayer snake game

---
+++ client.js
@@ -0,0 +1,6 @@
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541
+  });
+};
---
+++ play.js
@@ -0,0 +1,24 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: '50541'
+  });
+
+  // interpret incoming data as text
+
+  conn.on("connect", () => {
+    // code that does something when the connection is first established
+    console.log("You have connected!");
+    console.log("Welcome :)");
+  });
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
