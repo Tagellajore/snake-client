820f88475a3f06804ef4024d3391b1a4d277914d
Tagellajore <tagelsft9@gmail.com>
2023-05-10 17:11:01 -0400

    exported modules

--- client.js
+++ client.js
@@ -1,6 +1,22 @@
+const net = require("net");
+
+// establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
     host: '165.227.47.243',
-    port: 50541
+    port: '50541'
   });
+
+  // interpret incoming data as text
+
+  conn.on("connect", () => {
+    // code that does something when the connection is first established
+    console.log("You have connected!");
+    console.log("Welcome :)");
+  });
+  conn.setEncoding("utf8");
+
+  return conn;
 };
+
+module.exports = {connect};
--- play.js
+++ play.js
@@ -1,23 +1,6 @@
-const net = require("net");
+const {connect} = require("./client.js");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: '50541'
-  });
-
-  // interpret incoming data as text
-
-  conn.on("connect", () => {
-    // code that does something when the connection is first established
-    console.log("You have connected!");
-    console.log("Welcome :)");
-  });
-  conn.setEncoding("utf8");
-
-  return conn;
-};
+// establish a connection with game server
 
 console.log("Connecting ...");
 connect();
