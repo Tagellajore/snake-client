1f5196ff8dfc8f17fb8cfb0feb0a88687d254370
Tagellajore <tagelsft9@gmail.com>
2023-05-10 22:17:44 -0400

    added my initials on the snake

--- client.js
+++ client.js
@@ -8,15 +8,17 @@ const connect = function () {
   });
 
   // interpret incoming data as text
+  conn.setEncoding("utf8");
 
   conn.on("connect", () => {
     // code that does something when the connection is first established
-    console.log("You have connected!");
-    console.log("Welcome :)");
+    console.log("Successfully connected to game server ");
+    
   });
-  conn.setEncoding("utf8");
+  conn.write("Name: TAG");
 
   return conn;
 };
+
 
 module.exports = {connect};
