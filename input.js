66b17a45ffaa9fe5bbed4e7dfb1a5e5332a8d560
Tagellajore <tagelsft9@gmail.com>
2023-05-11 00:12:19 -0400

    sending some messages to peer

--- input.js
+++ input.js
@@ -17,7 +17,14 @@ const handleUserInput = function (key) {
   if (key === 'd') {
     connection.write("Move: right");
   }
+  if (key === '1') {
+    connection.write("Say: What's up");
+  }
+  if (key === '2') {
+    connection.write("Say: Good bye");
+  }
 };
+
 
 const setupInput =  (conn) => {
   connection = conn;
