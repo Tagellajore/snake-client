f35b36ee428760374a4f084981325db50c7cad88
Tagellajore <tagelsft9@gmail.com>
2023-05-10 23:44:16 -0400

    refactored play.js and added function to input.js

--- play.js
+++ play.js
@@ -1,23 +1,9 @@
 const {connect} = require("./client.js");
 
+const { setupInput } = require("./input");
+
 // establish a connection with game server
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  // stdin.on("data", handleUserInput);
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function () {
-  // your code here
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
 
 
 
