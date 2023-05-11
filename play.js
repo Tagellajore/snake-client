496776758e059c3cec257f9a2e76b1569fd8b810
Tagellajore <tagelsft9@gmail.com>
2023-05-10 23:37:06 -0400

    using ctrl + C for terminating the game

--- play.js
+++ play.js
@@ -2,6 +2,28 @@ const {connect} = require("./client.js");
 
 // establish a connection with game server
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  // stdin.on("data", handleUserInput);
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function () {
+  // your code here
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+
+
+
 console.log("Connecting ...");
 connect();
+
+setupInput();
 
